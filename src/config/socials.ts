4b29840524c4e2b4d637313e31dc50aa22f0ed8f
qlAD <3394543809@qq.com>
2022-04-30 17:36:15 +0800

    update sustats

--- src/config/socials.ts
+++ src/config/socials.ts
@@ -1,4 +1,4 @@
-import { Rss, Github, Props, Telegram,  Steam,} from '@icons-pack/react-simple-icons'
+import { Rss, Github, Props, Telegram,  Steam, E} from '@icons-pack/react-simple-icons'
 import { FC } from 'react'
 
 export interface SocialProps {
@@ -14,7 +14,7 @@ export const socials: SocialProps[] = [
     name: 'GitHub',
     link: 'https://github.com/qlAD',
     icon: Github,
-    apiUrl: 'https://api.spencerwoo.com/substats/?source=github&queryKey=spencerwooo',
+    apiUrl: 'https://api.swo.moe/stats/github/qlad',
     color: '#24292F',
   },
   {
@@ -24,6 +24,13 @@ export const socials: SocialProps[] = [
     apiUrl:
       'https://api.spencerwoo.com/substats/?source=feedly|inoreader&queryKey=https%3A%2F%2Fblog.spencerwoo.com%2Fposts%2Findex.xml',
     color: '#FFA500',
+  },
+  {
+    name: 'NeteaseMusic',
+    link: 'https://music.163.com/#/user/home?id=1933047728',
+    icon: E,
+    apiUrl: 'https://api.swo.moe/stats/neteasemusic/1933047728',
+    color: '#2CA5E0',
   },
   {
     name: 'Telegram',
